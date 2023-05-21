501911937e5274bacda47bd2febe3dbfc92ac266
Makar90 <Makarlancer@gmail.com>
2023-05-21 15:01:27 +0300

    router

--- front/src/common/Main/Main.jsx
+++ front/src/common/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 import './main.scss';
 import Home from '../../pages/home/Home';
--- front/src/index.js
+++ front/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './main.scss';
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter >
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
