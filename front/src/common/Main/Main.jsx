8bd1678375aef5866c6277a386e40a35b261dab7
Makar90 <Makarlancer@gmail.com>
2023-05-28 15:41:45 +0300

    fire base reg and login

--- front/src/common/Main/Main.jsx
+++ front/src/common/Main/Main.jsx
@@ -2,7 +2,7 @@ import { Routes, Route} from 'react-router-dom';
 import Menu from '../../components/Menu/Menu.jsx';
 
 import './main.scss';
-import Search from '../../pages/search/search.jsx';
+import Search from '../../pages/search/Search.jsx';
 import LoginPage from '../../pages/LoginPage/LoginPage.jsx';
 import RegistratePage from '../../pages/RegistratePage/RegistratePage.jsx';
 import ShopCart from '../../pages/shopcart/ShopCart.jsx';
