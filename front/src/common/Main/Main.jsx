3536bba639954e75fed0e5bf6ee2c1427978fa68
Makar90 <Makarlancer@gmail.com>
2023-06-03 12:14:37 +0300

    cute

--- front/src/common/Main/Main.jsx
+++ front/src/common/Main/Main.jsx
@@ -7,7 +7,7 @@ import LoginPage from '../../pages/Account/LoginPage/LoginPage.jsx';
 import RegistratePage from '../../pages/Account/RegistratePage/RegistratePage.jsx';
 import ShopCard from '../../pages/ShopCard/ShopCard.jsx';
 //import Home from '../../pages/Home/Home';
-import Shopers from '../../pages/Categories/shopers/Shopers';
+import Shopers from '../../pages/Categories/Shopers/Shopers.jsx';
 
 export default function Main(){
     return(
