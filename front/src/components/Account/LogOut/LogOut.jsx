643a21c35603427f44161cdc02b0833a92acfedf
Makar90 <Makarlancer@gmail.com>
2023-06-04 15:42:28 +0300

    c4

--- front/src/components/Account/LogOut/LogOut.jsx
+++ front/src/components/Account/LogOut/LogOut.jsx
@@ -1,17 +1,19 @@
-import { getAuth, /* onAuthStateChanged, */ signOut } from "firebase/auth";
-import {useNavigate} from 'react-router-dom';
+import { getAuth, signOut } from "firebase/auth";
+//import {useNavigate} from 'react-router-dom';
 import { removeUser_LocalStorage } from "../../../data/usersLocalStorage.jsx";
 
-export function LogOut(){
-    const navigate= useNavigate();
+function LogOut(){
+    //const navigate= useNavigate();
 
     const auth = getAuth();
     signOut(auth).then(() => {
         // Sign-out successful.
         removeUser_LocalStorage();
-        navigate ('/');
+        //navigate ('/');
         //window.location.reload(false);
     }).catch((error) => {
         // An error happened.
     });
 }
+
+export {LogOut}
